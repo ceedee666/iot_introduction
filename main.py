bd29133415190f4a0b9322882995dc9c53616ef9
Christian Drumm <drumm@fh-aachen.de>
2020-04-12 21:46:27 +0200

    moved wifi keys to a differnt file

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
 import sys
+sys.path.append("/src")
+
 import network
+import ntptime
 
-WIFI_SSID = "iPhone"
-WIFI_PW = "123Qwerty"
-
-
-def set_lib_path():
-    sys.path.append("/src")
-
+from aws_environment_data import *
 
 def connect_wifi():
+    wifi_passwds = {}
+    
+    with open("wifi_passwds.txt") as f:
+        for line in f.readlines():
+            wifi_id,passwd = line.split(":")
+            wifi_passwds[wifi_id]=passwd
+
+
     wlan = network.WLAN(network.STA_IF)
 
     if not wlan.isconnected():
         wlan.active(True)
-        print('connecting to network', WIFI_SSID)
-        wlan.connect(WIFI_SSID, WIFI_PW)
-        while not wlan.isconnected():
-            pass
 
-    print("connected:", wlan.ifconfig())
+        visible_wifis = [w[0] for w in wlan.scan()]
+        known_wifis = list(filter(lambda w: w in wifi_passwds.keys(), visible_wifis))
+
+        if len(known_wifis) > 0:
+            wifi = known_wifis[0]
+            print('connecting to network', wifi)
+            wlan.connect(wifi, wifi_passwds[wifi])
+            while not wlan.isconnected():
+                pass
+
+            print("connected:", wlan.ifconfig())
+        else:
+            print('No known network available.')
 
 
-set_lib_path()
-connect_wifi()
+def synchronize_rtc():
+    # set the rtc datetime from the remote server
+    ntptime.settime()
+
+if __name__ == "__main__":
+    connect_wifi()
+    synchronize_rtc()
+    go()
