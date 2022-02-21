1adee58f51048d21f2ebd7abe1c921ce72f3d4de
Christian Drumm <christian.drumm@hey.com>
2022-02-21 09:00:56 +0100

    fixed a typo

--- src/step_010_blinking_led.py
+++ src/step_010_blinking_led.py
@@ -4,7 +4,7 @@ from time import sleep_ms
 # ATTENTION: The pin numbers printed on the D1 mini and the NodeMCU
 #           are not the GPIO numbers used to initialize the pins
 #           in a python script.
-#           The mapping between the number on the micorcontroller
+#           The mapping between the number on the micro controller
 #           and the GPIO numbers can be found here
 #           https://docs.wemos.cc/en/latest/d1/d1_mini.html
 #           https://cdn.shopify.com/s/files/1/2930/7174/files/esp8266_devkit_horizontal-01.png
